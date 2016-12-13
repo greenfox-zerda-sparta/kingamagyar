de87d80120f951e4d1d811f84c69f2a807b55c04
Kinga Magyar <kinga.magyar89@gmail.com>
2016-12-13 16:04:12 +0100

    ex5 completed

--- Week_08/Day_02/05.cpp
+++ Week_08/Day_02/05.cpp
@@ -15,15 +15,23 @@ using namespace std;
 // Ron - Cloe
 // Mike - Joe
 
-void print_pairs(vector<string>& vec) {
-  random_shuffle(vec.begin(), vec.end());
-  for (vector<string>::iterator it = vec.begin(); it != vec.end(); it+=2) {
-    cout << *it << " - " << *(it+1) << endl;
+void print_pairs(vector<string>& vec1, vector<string>& vec2) {
+  random_shuffle(vec1.begin(), vec1.end());
+  random_shuffle(vec2.begin(), vec2.end());
+  for (unsigned int i = 0; i < vec1.size(); i++) {
+    for (unsigned int j = i; j < i+1; j++) {
+      if(vec1[i] == vec2[j]) {
+        random_shuffle(vec2.begin(), vec2.end());
+      } else {
+      cout << vec1[i] << " - " << vec2[j] << endl;
+      }
+    }
   }
 }
 
 void secret_santa() {
   vector<string> names;
+  vector<string> names2;
   string input;
   cout << "Input the names in your class, so I can make Secret Santa pairs! (press 'q' if you're done)" << endl;
   while(cin >> input) {
@@ -31,9 +39,10 @@ void secret_santa() {
       break;
     }
     names.push_back(input);
+    names2.push_back(input);
     cout << "Input the next name or press 'q' if you're done: " << endl;
   }
-  print_pairs(names);
+  print_pairs(names, names2);
 }
 
 int main() {
