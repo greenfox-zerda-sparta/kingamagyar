1496d0469dabf67a20a9c034b215bb8606c4466f
Kinga Magyar <kinga.magyar89@gmail.com>
2016-12-13 12:50:40 +0100

    ex 4

--- Week_08/Day_02/04.cpp
+++ Week_08/Day_02/04.cpp
@@ -9,8 +9,19 @@ using namespace std;
 // it should have integers as values that represents how many times the
 // letter appeared int the string
 
+map<string, int> modify(string a) {
+  map<string, int> m;
+  m [a] = a.size();
+  map<string, int>::iterator it;
+  for(it = m.begin(); it != m.end(); ++it) {
+    cout << it->first << " " << it->second << endl;
+  }
+  return m;
+}
+
 int main() {
-  
+  string a = "alma";
+  modify(a);
   
   return 0;
 }
