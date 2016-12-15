e898bed721f81c2c103ad73fc3661bc5d20d2d2e
Kinga Magyar <kinga.magyar89@gmail.com>
2016-12-15 10:45:03 +0100

    ex 3

---
+++ Week_08/Day_04_Recursion/03.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int sum_of_digits(unsigned int n) {
+  if (n == 0) {
+    return 0;
+  } else {
+    return (n % 10) + sum_of_digits(n / 10);
+  }
+}
+
+int main() {
+  // Given a non-negative int n,
+  // return the sum of its digits recursively (no loops).
+  // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
+  // while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+  unsigned int num = 284;
+  cout << sum_of_digits(num) << endl;
+  return 0;
+}
