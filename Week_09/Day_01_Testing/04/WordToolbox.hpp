79cb11ca5742eafc1be2988a5baafc8f5d7f36f9
Kinga Magyar <kinga.magyar89@gmail.com>
2016-12-19 17:08:30 +0100

    Exercise 04

---
+++ Week_09/Day_01_Testing/04/WordToolbox.hpp
@@ -0,0 +1,19 @@
+#ifndef WordToolbox_hpp
+#define WordToolbox_hpp
+
+#include <iostream>
+#include <algorithm>
+
+class WordToolbox {
+private:
+  std::string stringHeld;
+public:
+  WordToolbox(std::string stringHeld);
+  std::string get_stringHeld();
+  void set_stringHeld(std::string s);
+  bool isAnAnagram(std::string stringToCheck);
+  int countHowMany(char charToFind);
+  std::string convertToLowerCase(std::string str);
+};
+
+#endif /* WordToolbox_hpp */
