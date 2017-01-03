67f792a30de7e156ffa29821642be2ea71677ef3
Kinga Magyar <kinga.magyar89@gmail.com>
2017-01-03 11:11:34 +0100

    Added comments on what each line of code does

--- Week_10/Day_02/Example codes/client.cpp
+++ Week_10/Day_02/Example codes/client.cpp
@@ -3,14 +3,27 @@
 #include "SDL2_net/SDL_net.h"
 
 int main() {
+  //Initialize the SDL_net
   SDL_Init(SDL_INIT_EVERYTHING);
   SDLNet_Init();
+  
+  //Set-up the IP address of the client
   IPaddress ip;
   SDLNet_ResolveHost(&ip, "127.0.0.1", 1234);
+  
+  //Open client socket
   TCPsocket client = SDLNet_TCP_Open(&ip);
+  
+  //Allocate space for the text
   char text[100];
+  
+  //Receive the message
   SDLNet_TCP_Recv(client, text, 100);
+  
+  //Print out the message
   std::cout << text << std::endl;
+  
+  //Close the connection
   SDLNet_TCP_Close(client);
   SDL_Quit();
 }
--- Week_10/Day_02/Example codes/server.cpp
+++ Week_10/Day_02/Example codes/server.cpp
@@ -3,13 +3,23 @@
 #include "SDL2_net/SDL_net.h"
 
 int main() {
+  //Initialize the SDL_net
   SDL_Init(SDL_INIT_EVERYTHING);
   SDLNet_Init();
+  
+  //Set-up the IP address of the server to connect with
   IPaddress ip;
   SDLNet_ResolveHost(&ip, NULL, 1234);
+  
+  //Create server socket and client socket
   TCPsocket server = SDLNet_TCP_Open(&ip);
   TCPsocket client;
+  
+  //Create a text that we want to send to the client
   const char* text = "Alma";
+  
+  //Create an infinite loop where we wait for the client to connect
+  //if the client connects, we send the message and close the connection and break the infinite loop
   while (1) {
     client = SDLNet_TCP_Accept(server);
     if (client) {
